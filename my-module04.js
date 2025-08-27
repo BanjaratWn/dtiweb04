42ea1af017b3f52091cdaa832103eb5942cd1583
BanjaratWn <s6752410019@sau.ac.th>
2025-08-27 12:28:21 +0700

    Complete ESModule

---
+++ my-module04.js
@@ -0,0 +1,17 @@
+// สร้างแบบ CommonJS
+export const sauZZ = 'Southest Asia University'
+
+export let scoreZZ = 999
+
+export function sumNumberZZ(n1, n2){
+    return n1 + n2
+}
+
+//const squareAreaZZ = (wi, lo) => {
+//    return wi * lo
+//}
+//------------------------------------
+
+export default (wi, lo) => {
+    return wi * lo
+}
